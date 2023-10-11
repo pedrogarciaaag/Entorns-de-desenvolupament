857ace9a9ef2abf3fabc455c2a778c029b4ba0da
Pedro García <pgarcia12@nigul.cide.es>
2023-10-11 15:59:46 +0200

    Actividades

--- Actividad2/Actividad2.py
+++ Actividad2/Actividad2.py
@@ -1,5 +1,5 @@
 A = int(input("¿Cuanto vale A? : "))
-B = int(input ("¿Cuanto vale B? : "))
+B = int(input ("¿Cuanto vale B? :"))
 print ("Suma total = " , A+B)
 print("Resta total = " , A-B)
 print("Producto total = " , A*B)
---
+++ Actividad3/Actividad3.py
@@ -0,0 +1,8 @@
+A = int(input("¿Cuanto vale A? : "))
+B = int(input ("¿Cuanto vale B? : "))
+if A>B :
+    print(A , " es mayor")
+elif A == B :
+    print("Son iguales")
+else:
+    print(B , "es mayor")
