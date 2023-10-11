adb8426d0f09a116bd7d575cd6cbe93ed1931edc
Pedro García <pgarcia12@nigul.cide.es>
2023-10-11 15:50:37 +0200

    Actividades

---
+++ Actividad1/Actividad1.py
@@ -0,0 +1,3 @@
+A = input ("Introuzca A : ")
+B = input ("Introuzca B : ")
+print ("Valor de A es" , A , "Valor de B es" , B)
---
+++ Actividad2/Actividad2.py
@@ -0,0 +1,6 @@
+A = int(input("¿Cuanto vale A? : "))
+B = int(input ("¿Cuanto vale B? : "))
+print ("Suma total = " , A+B)
+print("Resta total = " , A-B)
+print("Producto total = " , A*B)
+print("Division total = " , A/B)
