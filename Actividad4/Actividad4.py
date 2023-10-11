59922e0713de148fdbccc91a366811f49f5042f8
Pedro García <pgarcia12@nigul.cide.es>
2023-10-11 16:09:01 +0200

    Actividad4

---
+++ Actividad4/Actividad4.py
@@ -0,0 +1,11 @@
+A = int(input("¿Cuanto vale A? : "))
+B = int(input ("¿Cuanto vale B? : "))
+C = int(input ("¿Cuanto vale C? : "))
+if A>B and A>C :
+    print(A, "es mayor")
+elif  A<B and A>=C :
+    print(B , "es mayor")
+elif C>A and C>=B :
+    print(C , "es mayor")
+elif A==C and A ==B :
+    print("Son iguales")
