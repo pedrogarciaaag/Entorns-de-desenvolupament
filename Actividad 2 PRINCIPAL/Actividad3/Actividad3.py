717344d17b7a3841056a4cfd93ac05dd20255804
Pedro García <pgarcia12@nigul.cide.es>
2023-10-18 15:57:43 +0200

    Actividad3

--- Actividad 2 PRINCIPAL/Actividad3/Actividad3.py
+++ Actividad 2 PRINCIPAL/Actividad3/Actividad3.py
@@ -2,11 +2,20 @@ Dia = int(input("Introduzca dia : "))
 Mes = int(input("Introduzca mes : ")) 
 if Dia>=1 and Dia<=31 :
     print("Dia valido")
+
     if Mes>=1 and Mes<=12 :
         print("Mes valido")
         print ("La fecha es valida")
+
     else :
         print("Mes no valido")
         print("La fecha es incorrecta")
-else :
-    print("Fecha es incorrecta")
+
+else : 
+    print("Dia no valido")
+    if Mes>=1 and Mes<=12 :
+        print("Mes valido")
+
+    else :
+        print("Mes no valido")
+    print ("La fecha es incorrecta")
